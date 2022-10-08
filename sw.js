0e79feac99f98809f57ce5b1a731c36766dbc029
Janusred <horaciok300@hotmail.com>
2022-10-08 01:47:12 -0500

    online basica

--- sw.js
+++ sw.js
@@ -1,3 +1,10 @@
 
+self.addEventListener('fetch', event =>{
 
+    const offlineResp = new Response(`Janus no hay internet`);
+
+    const resp = fetch(event.request).catch(()=> offlineResp);
+
+ event.respondWith(resp);
+});
 
