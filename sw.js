b1047e9fabaf6188966c8ce92dd989cd326c1868
Janusred <horaciok300@hotmail.com>
2022-10-10 00:47:25 -0500

    Li,pieza de cache dimanico

--- sw.js
+++ sw.js
@@ -1,9 +1,20 @@
 //const CACHE_NAME = 'cache-1';
 
-const CACHE_STATIC_NAME = 'static';
+const CACHE_STATIC_NAME = 'static-v2';
 const CACHE_DYNAMIC_NAME = 'dynamic-v1';
 
 const CACHE_INMUTABLE_NAME = 'inmutable-v1';
+
+
+function limpiarCache (cache_Name, numeroItem ){
+
+    caches.open(cacheNAme).then(cache=>{
+        return cache.keys()
+        .then(keys=>{
+            console.log(keys);
+        });
+    });
+}
 
 self.addEventListener('install', e =>{
  caches.open('caches-1').then(CACHE_STATIC_NAME =>{
@@ -39,6 +50,7 @@ console.log('No existe',e.request.url);
 return fetch( e.request).then(newResp=>{
     caches.open(CACHE_DYNAMIC_NAME =>{
         cache.put(e.request, newResp);
+        limpiarCache(CACHE_DYNAMIC_NAME, 5);
     });
     return newResp.clone();
    
